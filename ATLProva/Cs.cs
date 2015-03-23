02a057af8847c6f89f8ac881b0474fff63a9afab
nokipls <nokimrn@gmail.com>
2015-03-23 11:00:20 +0100

    FINAL sta cippa lololololol

--- ATLProva/Cs.cs
+++ ATLProva/Cs.cs
@@ -1,18 +1,19 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cs="http://cs/1.0">
+  <cs:CitySensing/>
   <cs:MonitoredCity name="Milano">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="13"/>
-    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="4"/>
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="3"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-      <langNumber value="5"/>
-      <langNumber key="English" value="6"/>
+      <langNumber value="0"/>
+      <langNumber key="English" value="1"/>
       <langNumber key="LassaSta" value="2"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
-      <postsPerTimeSlot value="1"/>
-      <postsPerTimeSlot key="Afternoon" value="2"/>
-      <postsPerTimeSlot key="Evening" value="3"/>
-      <postsPerTimeSlot key="Night" value="7"/>
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="1"/>
     </stats>
     <stats xsi:type="cs:CategoriesCount" name="#Posts per Category">
       <catNumber value="0"/>
@@ -21,31 +22,31 @@
     </stats>
   </cs:MonitoredCity>
   <cs:District name="SudOvest" coordEx="1.0" coordEy="1.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="3"/>
-    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="3"/>
-    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-      <langNumber value="0"/>
-      <langNumber key="English" value="2"/>
-      <langNumber key="LassaSta" value="1"/>
-    </stats>
-    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
-      <postsPerTimeSlot value="2"/>
-      <postsPerTimeSlot key="Afternoon" value="0"/>
-      <postsPerTimeSlot key="Evening" value="1"/>
-      <postsPerTimeSlot key="Night" value="0"/>
-    </stats>
-  </cs:District>
-  <cs:District name="SudCentro" coordBx="1.0" coordEx="2.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="1"/>
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
     <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
       <langNumber value="0"/>
       <langNumber key="English" value="1"/>
+      <langNumber key="LassaSta" value="1"/>
+    </stats>
+    <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
+      <postsPerTimeSlot value="0"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="1"/>
+    </stats>
+  </cs:District>
+  <cs:District name="SudCentro" coordBx="1.0" coordEx="2.0">
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
+    <stats xsi:type="cs:NumPostLang" name="#Posts per language">
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
       <langNumber key="LassaSta" value="0"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
       <postsPerTimeSlot value="0"/>
-      <postsPerTimeSlot key="Afternoon" value="1"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
       <postsPerTimeSlot key="Evening" value="0"/>
       <postsPerTimeSlot key="Night" value="0"/>
     </stats>
@@ -66,31 +67,31 @@
     </stats>
   </cs:District>
   <cs:District name="CentroOvest" coordBy="1.0" coordEx="1.0" coordEy="2.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
     <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
       <langNumber value="0"/>
-      <langNumber key="English" value="2"/>
+      <langNumber key="English" value="0"/>
       <langNumber key="LassaSta" value="0"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
       <postsPerTimeSlot value="0"/>
       <postsPerTimeSlot key="Afternoon" value="0"/>
       <postsPerTimeSlot key="Evening" value="0"/>
-      <postsPerTimeSlot key="Night" value="2"/>
+      <postsPerTimeSlot key="Night" value="0"/>
     </stats>
   </cs:District>
   <cs:District name="CentroCentro" coordBx="1.0" coordBy="1.0" coordEx="2.0" coordEy="2.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
+    <stats xsi:type="cs:NumPost" name="#Posts" metric="1"/>
     <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-      <langNumber value="1"/>
-      <langNumber key="English" value="1"/>
-      <langNumber key="LassaSta" value="0"/>
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
+      <langNumber key="LassaSta" value="1"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
       <postsPerTimeSlot value="0"/>
-      <postsPerTimeSlot key="Afternoon" value="1"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
       <postsPerTimeSlot key="Evening" value="0"/>
       <postsPerTimeSlot key="Night" value="0"/>
     </stats>
@@ -126,33 +127,33 @@
     </stats>
   </cs:District>
   <cs:District name="NordCentro" coordBx="1.0" coordBy="2.0" coordEx="2.0" coordEy="3.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
-    <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
+    <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-      <langNumber value="2"/>
+      <langNumber value="0"/>
       <langNumber key="English" value="0"/>
       <langNumber key="LassaSta" value="0"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
       <postsPerTimeSlot value="0"/>
       <postsPerTimeSlot key="Afternoon" value="0"/>
-      <postsPerTimeSlot key="Evening" value="1"/>
-      <postsPerTimeSlot key="Night" value="1"/>
+      <postsPerTimeSlot key="Evening" value="0"/>
+      <postsPerTimeSlot key="Night" value="0"/>
     </stats>
   </cs:District>
   <cs:District name="NordEst" coordBx="2.0" coordBy="2.0" coordEx="3.0" coordEy="3.0">
-    <stats xsi:type="cs:NumPost" name="#Posts" metric="4"/>
+    <stats xsi:type="cs:NumPost" name="#Posts"/>
     <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
     <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-      <langNumber value="2"/>
-      <langNumber key="English" value="2"/>
+      <langNumber value="0"/>
+      <langNumber key="English" value="0"/>
       <langNumber key="LassaSta" value="0"/>
     </stats>
     <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
       <postsPerTimeSlot value="0"/>
-      <postsPerTimeSlot key="Afternoon" value="1"/>
+      <postsPerTimeSlot key="Afternoon" value="0"/>
       <postsPerTimeSlot key="Evening" value="0"/>
-      <postsPerTimeSlot key="Night" value="3"/>
+      <postsPerTimeSlot key="Night" value="0"/>
     </stats>
   </cs:District>
   <cs:Place name="Gelateria Finocchi">
