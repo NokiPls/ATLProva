7f7ed77faf7586da638a819afe3f57c2ca9d6410
nokipls <nokimrn@gmail.com>
2015-04-13 17:33:19 +0200

    FINAL.

--- ATLProva/CitySensing.cs
+++ ATLProva/CitySensing.cs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <cs:CitySensing xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cs="http://cs/1.0">
   <cities name="Milano" coordEx="3.0" coordEy="3.0">
-    <districts name="SudOvest" coordEx="1.0" coordEy="1.0">
+    <districts name="SouthWest" coordEx="1.0" coordEy="1.0">
       <places name="IceCream4You">
         <stats xsi:type="cs:NumPost" name="#Posts"/>
         <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
@@ -42,26 +42,26 @@
           <catNumber key="Fun" value="0"/>
         </stats>
       </places>
-      <stats xsi:type="cs:NumPost" name="#Posts" metric="3"/>
-      <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="3"/>
+      <stats xsi:type="cs:NumPost" name="#Posts" metric="1"/>
+      <stats xsi:type="cs:NumPhoto" name="#Pictures" metric="1"/>
       <stats xsi:type="cs:NumPostLang" name="#Posts per language">
         <langNumber value="0"/>
-        <langNumber key="English" value="2"/>
+        <langNumber key="English" value="0"/>
         <langNumber key="Italian" value="1"/>
       </stats>
       <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
-        <postsPerTimeSlot value="2"/>
+        <postsPerTimeSlot value="0"/>
         <postsPerTimeSlot key="Afternoon" value="0"/>
         <postsPerTimeSlot key="Evening" value="1"/>
         <postsPerTimeSlot key="Night" value="0"/>
       </stats>
       <stats xsi:type="cs:CategoriesCount" name="#Posts per Category">
         <catNumber value="0"/>
-        <catNumber key="Food" value="2"/>
+        <catNumber key="Food" value="0"/>
         <catNumber key="Fun" value="0"/>
       </stats>
     </districts>
-    <districts name="South" coordBx="1.0" coordEx="2.0">
+    <districts name="South" coordBx="1.0" coordEx="2.0" coordEy="1.0">
       <stats xsi:type="cs:NumPost" name="#Posts"/>
       <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
       <stats xsi:type="cs:NumPostLang" name="#Posts per language">
@@ -162,23 +162,23 @@
       </stats>
     </districts>
     <districts name="NorthWest" coordBy="2.0" coordEx="1.0" coordEy="3.0">
-      <places name="Pizzeria">
-        <stats xsi:type="cs:NumPost" name="#Posts"/>
+      <places name="Forno Ambrosiano">
+        <stats xsi:type="cs:NumPost" name="#Posts" metric="2"/>
         <stats xsi:type="cs:NumPhoto" name="#Pictures"/>
         <stats xsi:type="cs:NumPostLang" name="#Posts per language">
-          <langNumber value="0"/>
-          <langNumber key="English" value="0"/>
+          <langNumber value="1"/>
+          <langNumber key="English" value="1"/>
           <langNumber key="Italian" value="0"/>
         </stats>
         <stats xsi:type="cs:NumPostTimeSlot" name="#Posts per time slot">
           <postsPerTimeSlot value="0"/>
-          <postsPerTimeSlot key="Afternoon" value="0"/>
+          <postsPerTimeSlot key="Afternoon" value="1"/>
           <postsPerTimeSlot key="Evening" value="0"/>
-          <postsPerTimeSlot key="Night" value="0"/>
+          <postsPerTimeSlot key="Night" value="1"/>
         </stats>
         <stats xsi:type="cs:CategoriesCount" name="#Posts per Category">
           <catNumber value="0"/>
-          <catNumber key="Food" value="0"/>
+          <catNumber key="Food" value="1"/>
           <catNumber key="Fun" value="0"/>
         </stats>
       </places>
